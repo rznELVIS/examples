f717734694d448b01fd87f135aa7f7a8aca1f6c5
Дмитрий Демидов <dmitri-demidiv@yandex.ru>
2026-03-23 22:06:43 +0300

    Useless code is removed

--- DistributedLocks/MutexAccess/MutexService.cs
+++ DistributedLocks/MutexAccess/MutexService.cs
@@ -2,7 +2,7 @@ namespace MutexAccess;
 
 public class MutexService : IDisposable
 {
-    public readonly Mutex CurrentMutex = new Mutex(false, "Global\\Counter250");
+    public readonly Mutex CurrentMutex = new Mutex(false, "Global\\Counter");
     
     public void Dispose()
     {
